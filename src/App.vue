16bab66054e0cd0ffffa5b4c0a72c14acc8e1b6d
Mohammad Saeed Roohbakhsh <50274134+msrk81@users.noreply.github.com>
2022-12-08 13:10:17 +0330

    starting code

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import "style/app/fonts.css";
+@import "style/app/flexbox.css";
+@import "style/app/prefix.css";
+</style>
